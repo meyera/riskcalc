8dc6cb8fdf3ce1ef4ec66f69daebc8c80fcc5ba2
Alexander Meyer <meyera@users.noreply.github.com>
2015-03-17 21:14:57 +0100

    initial commit

---
+++ R/euroscore.R
@@ -0,0 +1,1 @@
+#ESI and II
---
+++ R/hello.R
@@ -0,0 +1,25 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+Liebe Frau Strietzel,
+vielen Dank für Ihre nette Unterstützung und Mühe. Am Samstag erfuhren wir überraschender Weise, dass unsere Tochter nun doch zu unseren Erstwunsch-Gymnasium südlich im Bezirk Tiergarten zugelassen wurde. Bis dahin waren alle Weichen auf das Heinrich Schliemann Gymnasium am Prenzlauer Berg gestellt. Nach intensiver Überlegung haben wir uns entschieden, eine Wohnung in der Nähe der Schule zu suchen. Einen Schulweg von ~45 Minuten 2x/Tag wollen wir unserer Tochter nicht zumuten.
+
+Sehr schade... die Wohnung und La Vie hat uns sehr zugesagt. Ich danke Ihnen sehr für Ihre Mühe und bitte um Verständnis für den Entscheidungswechsel.
+Mit freundlichen Grüßen,
+Ihr Alexander Meyer
